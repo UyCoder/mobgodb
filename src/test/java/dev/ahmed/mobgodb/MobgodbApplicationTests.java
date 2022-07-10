87a74367b4d344eecaec1c34b944ab0bf4a666d3
uycoder <Thunder1987>
2022-07-10 22:01:48 +0300

    finished video 69, 70

--- src/test/java/dev/ahmed/mobgodb/MobgodbApplicationTests.java
+++ src/test/java/dev/ahmed/mobgodb/MobgodbApplicationTests.java
@@ -1,6 +1,7 @@
 package dev.ahmed.mobgodb;
 
 import com.github.javafaker.Faker;
+import com.mongodb.client.result.UpdateResult;
 import dev.ahmed.mobgodb.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
 import java.util.List;
 import java.util.regex.Pattern;
@@ -110,5 +112,39 @@ class MobgodbApplicationTests {
                 query.skip((pageNo - 1) * pageSize).limit(pageSize), User.class);
         System.out.println(count);
         System.out.println(users1);
+    }
+
+    // update user test
+    @Test
+    void updateUser() {
+        // 1. find user by id
+        User user = mongoTemplate.findById("62cb165fc9539c62bb674c98", User.class);
+
+        // 2. update user
+        user.setName("Ahmed Bughra");
+        user.setAge(31);
+        user.setEmail("ahmedcan@gmail.com");
+
+        // 3. save user
+        Query query = new Query(Criteria.where("_id").is(user.getId()));
+        Update update = new Update().set("name", user.getName()).set("age", user.getAge());
+
+        update.set("name", user.getName());
+        update.set("age", user.getAge());
+        update.set("email", user.getEmail());
+
+        UpdateResult upsert = mongoTemplate.upsert(query, update, User.class);
+        long modifiedCount = upsert.getModifiedCount();
+        System.out.println(modifiedCount);
+        System.out.println(user);
+        System.out.println(upsert);
+    }
+
+
+
+    // delete user test
+    @Test
+    void deleteUser() {
+        mongoTemplate.remove(new Query(Criteria.where("name").is("Ahmed-Malvina")), User.class);
     }
 }
