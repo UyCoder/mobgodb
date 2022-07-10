a29d2366468f9d33fb15a51faf9ab38f73eecc19
uycoder <Thunder1987>
2022-07-10 21:47:49 +0300

    finished video 69

--- src/test/java/dev/ahmed/mobgodb/MobgodbApplicationTests.java
+++ src/test/java/dev/ahmed/mobgodb/MobgodbApplicationTests.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 @SpringBootTest
 class MobgodbApplicationTests {
@@ -21,8 +24,8 @@ class MobgodbApplicationTests {
     @Test
     void create() {
         User user = new User();
-        user.setName("Ahmed-"+faker.name().firstName());
-        user.setAge(18 + (int)(Math.random() * 63));
+        user.setName("Ahmed-" + faker.name().firstName());
+        user.setAge(18 + (int) (Math.random() * 63));
         user.setEmail("ahmedbughra@gmail.com");
         User user1 = mongoTemplate.insert(user);
         System.out.println(user1);
@@ -31,8 +34,81 @@ class MobgodbApplicationTests {
     // findall data test
     @Test
     void findAll() {
-        List<User> all  = mongoTemplate.findAll(User.class);
+        List<User> all = mongoTemplate.findAll(User.class);
         System.out.println(all);
     }
 
+    // findbyid data test
+    @Test
+    void findById() {
+        User user = mongoTemplate.findById("62cb174fd8e5a82b9876edf0", User.class);
+        System.out.println(user);
+    }
+
+    // find user list test
+    @Test
+    void findUserList() {
+        // name=test and age=25
+        Query query = new Query(
+                Criteria.where("name").is("Ahmed").and("age").is(25));
+        List<User> users = mongoTemplate.find(query, User.class);
+        System.out.println(users);
+    }
+
+    // find like user list test
+    @Test
+    void findLikeUserList() {
+        // name like ahmed and age=25
+        Query query = new Query(
+                Criteria.where("name").regex("^Ahmed"));
+        List<User> users = mongoTemplate.find(query, User.class);
+        System.out.println(users);
+    }
+
+    // second way to find like user list test
+    @Test
+    void findLikeUserList2() {
+        // name like ahmed
+        String name = "hme";
+        String regex = String.format("%s%s%s", "^.*", name, ".*$");
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Query query = new Query(
+                Criteria.where("name").regex(pattern));
+        List<User> users = mongoTemplate.find(query, User.class);
+        System.out.println(users);
+    }
+
+    // third way to find like user list test
+    @Test
+    void findLikeUserList3() {
+        // name like ahmed
+        String regex = "hme";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Query query = new Query(
+                Criteria.where("name").regex(pattern));
+        List<User> users = mongoTemplate.find(query, User.class);
+        System.out.println(users);
+    }
+
+    // page user list test
+    @Test
+    void pageUserList() {
+        int pageNo = 1;
+        int pageSize = 3;
+        String name = "Ahmed";
+
+        String regix = String.format("%s%s%s", "^.*", name, ".*$");
+        Pattern pattern = Pattern.compile(regix, Pattern.CASE_INSENSITIVE);
+        Query query = new Query(
+                Criteria.where("name").regex(pattern));
+
+        // page
+        // search count
+        long count = mongoTemplate.count(query, User.class);
+        // page search
+        List<User> users1 = mongoTemplate.find(
+                query.skip((pageNo - 1) * pageSize).limit(pageSize), User.class);
+        System.out.println(count);
+        System.out.println(users1);
+    }
 }
